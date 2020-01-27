781df9f1dcbb995b4518395ef209bc64a23ba77c
lina-6020 <59893804+lina-6020@users.noreply.github.com>
2020-01-26 19:56:16 -0500

    Update CalculadoraComplejos.py

--- CalculadoraComplejos.py
+++ CalculadoraComplejos.py
@@ -1,4 +1,4 @@
-from sys import stdin
+import math 
 def sumar(numero1,numero2):
     resul1=numero1[0]+numero2[0]
     resul2=numero1[1]+numero2[1]
@@ -6,7 +6,7 @@ def sumar(numero1,numero2):
 def restar(numero1,numero2):
     resul1=numero1[0]-numero2[0]
     resul2=numero1[1]-numero2[1]
-    print(resul1,resul2)
+    return resul1,resul2
 def multiplicar(numero1,numero2):
     a=numero1[0]*numero2[0]
     b=numero1[0]*numero2[1]
@@ -14,11 +14,14 @@ def multiplicar(numero1,numero2):
     d=numero1[1]*numero2[1]
     e=b+c
     f=a+d*-1
-    print(f,e)
+    return f,e
 def conjugado(numero1):
     return numero1[0]*-1,numero1[1]*-1
+def modulo(numero1):
+    return (numero1[0]**2+numero1[1]**2)**0.5
 def dividir(numero1,numero2):
-    numero3=conjugado(numero2)
+    numero3=[]
+    numero3=[numero2[0],numero2[1]*-1]
     a=numero1[0]*numero3[0]
     b=numero1[0]*numero3[1]
     c=numero1[1]*numero3[0]
@@ -31,32 +34,21 @@ def dividir(numero1,numero2):
     j=numero2[1]*numero3[1]
     k=h+i
     l=g+j*-1
-    print(e,f)
-    print(k,l)
-def sumavector(vector1,vector2):
-    vectorfinal=[]
-    if len(vector1)==len(vector2):
-        for i in range (len(vector1)):
-            vectorfinal.append(sumar(vector1[i],vector2[i]))
-    return vectorfinal
-def inversovector(vector1):
-    vectorfinal=[]
-    for i in range (len(vector1)):
-        vectorfinal.append(conjugado(vector1[i]))
-    print(vectorfinal)
-    
+    resultado=(f/l,e/l)
+    return resultado
 
-
-def main():
-    vector1=[[-2.0, 3.0], [4.0, -5.0]]
-    vector2=[[6.0, 7.0], [8.0, 9.0]]
-##    numero1=([float(x) for x in stdin.readline().split(",")])
-##    numero2=([float(x) for x in stdin.readline().split(",")])
-##    sumar(vector1,vector2)
-##    restar(numero1,numero2)
-##    multiplicar(numero1,numero2)
-##    conjugado(numero1)
-##    dividir(numero1,numero2)
-    sumavector(vector1,vector2)
-    inversovector(vector1)
-main()
+def polar_cartesiano(numero1):
+    r=numero1[0]
+    anguloradianes=numero1[1]*(math.pi/180)
+    x=r*math.cos(anguloradianes)
+    y=r*math.sin(anguloradianes)
+    return x,y
+def cartesiano_polar(numero2):
+    r=(numero2[0]**2+numero2[1]**2)**0.5
+    angulo=math.atan(numero2[1]/numero2[0])
+    angulor=angulo*(180/math.pi)
+    return r,angulor
+def fase(numero1):
+    fase=math.atan(numero1[1]/numero1[0])
+    faseg=fase*(180/math.pi)
+    return faseg()
