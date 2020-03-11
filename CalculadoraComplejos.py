f6285c406a071fd0ace014eaf62714c56c60db12
unknown <lina.buitrago@labinfo.is.escuelaing.edu.co>
2020-03-11 14:26:28 -0500

    sistema de una particula

--- CalculadoraComplejos.py
+++ CalculadoraComplejos.py
@@ -207,11 +207,7 @@ def probabilidad (pos,vector):
 def amplitudTransicion(matriz,vector):
     a = vectorxmatriz(vector,matriz)
     conjugado = inversovector(a)
-    ans = producto_vectores(conjugado,vector)
-    return ans 
-def main():
-    vector=[[2,1], [-1,2], [0,1], [1,0], [3,-1], [2,0], [0,-2], [-2,1], [1,-3], [0,-1]]
-    pos=7
-    print(probabilidad(pos,vector))
-main()
+    respuesta = producto_vectores(conjugado,vector)
+    return respuesta 
+
 
