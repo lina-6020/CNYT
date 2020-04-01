11fa676a2a922aa6d37f88fe2aaff1a5be549f61
Lina Buitrago <lina.buitrago@mail.escuelaing.edu.co>
2020-04-01 13:28:11 -0500

    ok

--- SimulacionDeLoClasicoALoCuantico.py
+++ SimulacionDeLoClasicoALoCuantico.py
@@ -1,11 +1,11 @@
 from CalculadoraComplejos import *
 def ExperimentoCanicas(vectorEstado,canicas,nclicks):
     for i in range(nclicks):
-        vectorEstado = vectorxmatrizReales(vectorEstado,canicas)
+        vectorEstado = vectormatrizReal(vectorEstado,canicas)
     return vectorEstado
 def ExperimentoRendijasProb(nrendijas,resultado,inicio,vectorEstado):
     for i in range(nrendijas):
-            inicio = producto_matricesReales(inicio,inicio)
-    respuesta=vectorxmatrizReales(vectorEstado,inicio)
-        return respuesta
+            inicio = producto_matrices_reales(inicio,inicio)
+    respuesta=vectormatrizReal(vectorEstado,inicio)
+    return respuesta
 "La funcion es la misma para cuantico y probabilistico ya que la funcion sirve para n rendijas"
